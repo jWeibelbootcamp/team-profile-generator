3d8c9e7eb9169458a6fea8283c72687f41dd446b
devWeibel <weibel.jason@gmail.com>
2022-10-16 23:34:04 -0400

    require variables in index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
