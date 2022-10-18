4f58cda2eccb7b0144ce685d7ec184c6a8bd6e14
devWeibel <weibel.jason@gmail.com>
2022-10-17 22:00:49 -0400

    wrote index.js questions

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+const team = [];
+
+const employeeQ = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter Employee Name'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter Employee ID'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter Employee Email'
+    }
+];
+
+const managerQ = [
+    {
+        type: 'input',
+        name: 'office',
+        message: 'Enter Manager Office Number'
+    }
+];
+
+const engineerQ = [
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'Enter Engineer GitHub'
+    }
+];
+
+const internQ = [
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Enter Intern School'
+    }
+];
